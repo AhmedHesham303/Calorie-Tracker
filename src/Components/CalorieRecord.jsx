abd764ebf4d1d39784d9f9765dd8b47e005b2e7e
AhmedHesham <ahmed.hesham303303@gmail.com>
2025-04-30 02:52:14 +0300

    Adding CalorieRecord dummy data

--- src/Components/CalorieRecord.jsx
+++ src/Components/CalorieRecord.jsx
@@ -1,1 +1,10 @@
-function CalorieRecord() {}
+export default function CalorieRecord() {
+  return (
+    <li>
+      <ul> 340 calorie</ul>
+      <ul>Breakfast</ul>
+      <ul>Eggs + Beans</ul>
+      <ul>{Date.now()}</ul>
+    </li>
+  );
+}
