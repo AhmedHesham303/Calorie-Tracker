a8e48d7fb1cf30f92d5e2e536041bcad4f46b518
AhmedHesham <ahmed.hesham303303@gmail.com>
2025-06-24 00:24:01 +0300

    adding and importing css files

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import CalorieRecord from "./Components/CalorieRecord";
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
       <CalorieRecord></CalorieRecord>
     </div>
   );
--- src/Components/CalorieRecord.jsx
+++ src/Components/CalorieRecord.jsx
@@ -1,10 +1,12 @@
+import "./CalorieRecord.css";
 function CalorieRecord() {
   return (
-    <ul>
-      <li> 340 calorie</li>
+    <ul className="record">
+      <li>March 6, 2023</li>
       <li>Breakfast</li>
       <li>Eggs + Beans</li>
-      <li>{Date.now()}</li>
+
+      <li className="record-calories">340</li>
     </ul>
   );
 }
