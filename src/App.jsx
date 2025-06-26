6f79066059e73a1fb1cee0a9d1464ff39c6458b8
AhmedHesham <ahmed.hesham303303@gmail.com>
2025-06-26 04:55:22 +0300

    making records as a state to allow adding new meal

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,38 @@ import CalorieRecord from "./Components/calorieRecordSection/CalorieRecord";
 import CaloriesRecordEdit from "./Components/edit/CaloriesRecordEdit";
 import RecordList from "./Components/calorieRecordSection/RecordList";
 import "./Components/calorieRecordSection/RecordList.css"; // move this to the top or after imports
-
+import { useState } from "react";
+const INITIAL_RECORDS = [
+  {
+    date: new Date(2023, 6, 24),
+    meal: "breakfalst",
+    calories: 340,
+    content: "eggs",
+  },
+  {
+    date: new Date(2023, 6, 24),
+    meal: "breakfalst",
+    calories: 340,
+    content: "eggs",
+  },
+  {
+    date: new Date(2023, 6, 24),
+    meal: "breakfalst",
+    calories: 340,
+    content: "eggs",
+  },
+  {
+    date: new Date(2023, 6, 24),
+    meal: "breakfalst",
+    calories: 340,
+    content: "eggs",
+  },
+];
 export default function App() {
-  const records = [
-    {
-      date: new Date(2023, 6, 24),
-      meal: "breakfalst",
-      calories: 340,
-      content: "eggs",
-    },
-    {
-      date: new Date(2023, 6, 24),
-      meal: "breakfalst",
-      calories: 340,
-      content: "eggs",
-    },
-    {
-      date: new Date(2023, 6, 24),
-      meal: "breakfalst",
-      calories: 340,
-      content: "eggs",
-    },
-    {
-      date: new Date(2023, 6, 24),
-      meal: "breakfalst",
-      calories: 340,
-      content: "eggs",
-    },
-  ];
+  const [records, setRecords] = useState(INITIAL_RECORDS);
 
   const formSubmitHandler = function (record) {
+    setRecords([record, ...record]);
     console.log(record);
   };
   return (
