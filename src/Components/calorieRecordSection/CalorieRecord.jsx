f1140d6bb17e947238ef95f11bc2d8e1c0d1a141
AhmedHesham <ahmed.hesham303303@gmail.com>
2025-06-29 21:14:37 +0300

    allowing condetional rendering according to calories value

--- src/Components/calorieRecordSection/CalorieRecord.jsx
+++ src/Components/calorieRecordSection/CalorieRecord.jsx
@@ -4,14 +4,23 @@ import StyledRecordCell from "../common/StyledRecordCell";
 import CalorieDateRecord from "./CalorieDateRecord";
 import { useState } from "react";
 export default function CalorieRecord(props) {
-  const [calories, setCalories] = useState(props.calories);
+  let recordContent = (
+    <>
+      <li>{props.meal}</li>
+      <li>{props.content}</li>
+    </>
+  );
+  if (props.calories < 0) {
+    recordContent = (
+      <>
+        <li></li>
+        <li>Invalid calories</li>
+      </>
+    );
+  }
   const month = props.date.toLocaleString("default", { month: "long" });
   const year = props.date.getFullYear();
   const day = props.date.getDate();
-
-  const caloriesCkickHandler = function () {
-    setCalories(calories + 10);
-  };
   return (
     <ul className="record">
       <li>
@@ -21,11 +30,9 @@ export default function CalorieRecord(props) {
           day={day}
         ></CalorieDateRecord>
       </li>
-      <li>{props.meal}</li>
-      <li>{props.content}</li>
-
-      <li className="record-calories" onClick={caloriesCkickHandler}>
-        <StyledRecordCell>{calories}</StyledRecordCell>
+      {recordContent}
+      <li className="record-calories">
+        <StyledRecordCell>{props.calories}</StyledRecordCell>
       </li>
     </ul>
   );
